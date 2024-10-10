f84a9455a10f1ba726f51e3b18c39700c5db6620
Iris Lee <irislee4git@gmail.com>
2024-10-10 14:35:45 -0400

    splash set up

---
+++ WikiWander/client/src/components/splash/Splash.jsx
@@ -0,0 +1,10 @@
+import { Container } from "reactstrap"
+
+export const Splash = () => {
+
+    return(
+        <Container>
+            <h4>WIKI WANDER</h4>
+        </Container>
+    )
+}
--- WikiWander/client/src/views/ApplicationViews.jsx
+++ WikiWander/client/src/views/ApplicationViews.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
+import { Splash } from "../components/splash/Splash.jsx"
 import { Game } from "../components/game/Game.jsx"
 
 export const ApplicationViews = () => {
     return(
        <Routes>
-            <Route path="/" element={<Game />} />
+            <Route path="/" element={<Splash />} />
+            <Route path="game" element={<Game />} />
         </Routes>
     )
 }
