e07a97e0dfff68431f407aa9ab66d4b59694d0ed
Iris Lee <irislee4git@gmail.com>
2024-10-11 12:02:06 -0400

    start and end articles

--- WikiWander/client/src/components/game/Game.jsx
+++ WikiWander/client/src/components/game/Game.jsx
@@ -3,18 +3,63 @@
 import { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import { Button, Col } from "reactstrap";
+import { getAllArticles } from "../../services/articleService.jsx";
 
 export const Game = () => {
 const [page, setPage] = useState({});
 const [gameActivated, setGameActivated] = useState(false);
 const [articles, setArticles] = useState([]);
-const [startArticle, setStartArticle] = useState('');
-const [endArticle, setEndArticle] = useState('');
+const [startArticle, setStartArticle] = useState({});
+const [endArticle, setEndArticle] = useState({});
 
 
-const pickArticles = () => {
+const getAndShuffleArticles = async () => {
 
-}
+  const shuffle = (array) => {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+
+  await getAllArticles()
+    .then(articleArray => {
+      shuffle(articleArray)
+  setArticles(articleArray)
+  })
+
+  console.log(articles[0]);
+};
+
+useEffect(() => {
+  getAndShuffleArticles();
+},[])
+
+// const shuffle = (array) => {
+//   let currentIndex = array.length;
+
+//   // While there remain elements to shuffle...
+//   while (currentIndex != 0) {
+
+//     // Pick a remaining element...
+//     let randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex--;
+
+//     // And swap it with the current element.
+//     [array[currentIndex], array[randomIndex]] = [
+//       array[randomIndex], array[currentIndex]];
+//   }
+// }
+
+const setArticlesForGame = () => {
+  const firstArticle = articles[0]
+  const secondArticle = articles[1]
+
+  setStartArticle(firstArticle);
+  setEndArticle(secondArticle);
+  };
 
   // useEffect(() => {
   //   if(page !== null) {
@@ -54,10 +99,19 @@ const pickArticles = () => {
       <h1>NEW GAME</h1>
           <Row xs={2}>
             <Col>
-              testing
-              place start and end articles here
+              <h3>Start Article</h3>
+              <p>{startArticle?.name}</p>
+              <h3>End Article</h3>
+              <p>{endArticle?.name}</p>
             </Col>
             <Col>
+            {/* button to start game by picking articles */}
+            <Button
+            onClick={setArticlesForGame}
+            >
+              Push for Articles
+            </Button>
+            {/* have button show when game activated and articles are set */}
             {gameActivated === false && (
                 <Button
               onClick={showPage}
