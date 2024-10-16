fa5be333565a35a5479fb1b977b9a6758b55d516
Iris Lee <irislee4git@gmail.com>
2024-10-15 20:30:11 -0400

    crawls page to page

--- WikiWander/client/src/components/game/GameSheet.jsx
+++ WikiWander/client/src/components/game/GameSheet.jsx
@@ -1,4 +1,5 @@
 import { useRef, useEffect } from "react";
+import "./GameSheet.css"
 
 export const GameSheet = ({ board, fetchPage }) => {
   const myRef = useRef(null);
@@ -6,7 +7,9 @@ export const GameSheet = ({ board, fetchPage }) => {
   useEffect(() => {
     if(board){
     let newBoard = board
-     newBoard = newBoard.replace(/href/g, "name");
+    //  newBoard = newBoard.replace(/href/g, "name");
+    // newBoard = newBoard.replace(/title/g, "name");
+    newBoard = newBoard.replace(/href="\/wiki\//g, 'class="clickable" name="');
 
     myRef.current.innerHTML = newBoard;
     console.log(newBoard);
@@ -16,9 +19,12 @@ export const GameSheet = ({ board, fetchPage }) => {
   return (
     <div onClick={(e) => 
         {
-         fetchPage(e.target.name.split("/")[2])   
+            // e.preventDefault();
+            fetchPage(e.target.name);
+            //  fetchPage(e.target.name.split("/")[2]);   
     }}>
       <span ref={myRef} />
     </div>
   );
 };
+
